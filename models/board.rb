b0086b029525ae967b4988d9ec6b948cdde89fdf
Dexter Sealy <dextersealy@gmail.com>
2017-07-10 00:45:44 -0400

    Rollback inline C code for heroku

--- models/board.rb
+++ models/board.rb
@@ -31,7 +31,7 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    raise InvalidPosition.new unless [start_pos, end_pos].all? { |pos| in_bounds_c(pos) }
+    raise InvalidPosition.new unless [start_pos, end_pos].all? { |pos| in_bounds?(pos) }
     raise NoPiece.new if self[start_pos].is_a?(NullPiece)
 
     @undo << [start_pos, end_pos, self[end_pos]]
@@ -62,17 +62,27 @@ class Board
     grid[row][col] = piece
   end
 
-  inline do |builder|
-    builder.c "
-      VALUE in_bounds_c(VALUE pos) {
-        int row = NUM2INT(rb_ary_entry(pos, 0));
-        int col = NUM2INT(rb_ary_entry(pos, 1));
-        if (row >= 0 && row < 8 && col >= 0 && col < 8) {
-          return Qtrue;
-        } else {
-          return Qfalse;
-        }
-      }"
+  def in_bounds(pos)
+    row, col = pos
+    row >= 0 && row < 8 && col >= 0 && col < 8
+  end
+
+  # inline do |builder|
+  #   builder.c "
+  #     VALUE in_bounds(VALUE pos) {
+  #       int row = NUM2INT(rb_ary_entry(pos, 0));
+  #       int col = NUM2INT(rb_ary_entry(pos, 1));
+  #       if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+  #         return Qtrue;
+  #       } else {
+  #         return Qfalse;
+  #       }
+  #     }"
+  # end
+
+  def in_bounds?(pos)
+    row, col = pos
+    row >= 0 && col >= 0 && row < 8 && col < 8
   end
 
   def occupied?(pos)
