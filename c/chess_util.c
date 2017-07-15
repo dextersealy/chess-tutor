92a58ed199520c2f5a808af2585c0d56270199f5
Dexter Sealy <dextersealy@gmail.com>
2017-07-15 10:28:30 -0400

    Refactor C module

--- c/chess_util.c
+++ c/chess_util.c
@@ -4,27 +4,29 @@
 
 //  H e l p e r   f u n c t i o n s
 
-//  Retrieve color of board piece
+static VALUE get_board_ary(VALUE piece) {
+  return rb_iv_get(rb_iv_get(piece, "@board"), "@board");
+}
 
-char get_color_at(VALUE board, int row, int col) {
-  VALUE iv_board = rb_iv_get(board, "@board");
-  VALUE piece = rb_ary_entry(iv_board, row * 8 + col);
+//  Get color of piece
+
+static char get_color_at(VALUE board_ary, int row, int col) {
+  VALUE piece = rb_ary_entry(board_ary, row * 8 + col);
   VALUE color = rb_iv_get(piece, "@color");
   return (TYPE(color) == T_NIL) ? ' ' : *rb_id2name(SYM2ID(color));
 }
 
-//  Test if piece occupies board position
+//  Test if board position contains a piece
 
-int is_occupied(VALUE board, int row, int col) {
-  return get_color_at(board, row, col) != ' ';
+static int is_occupied(VALUE board_ary, int row, int col) {
+  return get_color_at(board_ary, row, col) != ' ';
 }
 
-//  Test if board position is valid and is empty or is_occupied by
-//  an opposing piece
+//  Test if board position is valid move for a color
 
-int is_valid_pos(VALUE board, int row, int col, char piece) {
+int is_valid_pos(VALUE board_ary, int row, int col, char color) {
   return 0 <= row && row < 8 && 0 <= col && col < 8 &&
-    piece != get_color_at(board, row, col);
+    color != get_color_at(board_ary, row, col);
 }
 
 //  Append position to Ruby array
@@ -124,7 +126,7 @@ static struct piece_table {
   }}
 };
 
-//  Retrieve the table for a piece
+//  Get the table for a piece
 
 struct piece_table *get_piece_table(const char *type) {
   for (int i = 0, n = sizeof(PIECE_TABLES) / sizeof(PIECE_TABLES[0]); i < n; i++) {
@@ -161,6 +163,26 @@ int get_piece_value(VALUE piece, int offset, const char *player) {
   }
 
   return value;
+}
+
+//  Test whether the board position contains a Rook that had never moved
+
+static int rook_and_not_moved(VALUE ary, int row, int col) {
+  VALUE rook = rb_ary_entry(ary, row * 8 + col);
+  return strcmp(rb_obj_classname(rook), "Rook") == 0 &&
+    !RTEST(rb_iv_get(rook, "@moved"));
+}
+
+//  Test whether a series of board postions is unoccupied
+
+static int not_occupied(VALUE ary, int row, int start_col, int end_col) {
+  for (int i = start_col; i <= end_col; i++) {
+    VALUE piece = rb_ary_entry(ary, row * 8 + i);
+    if (TYPE(rb_iv_get(piece, "@color")) != T_NIL) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 //  M o d u l e   i n t e r f a c e
@@ -204,22 +226,22 @@ static VALUE get_moves(int argc, VALUE *argv, VALUE self) {
   VALUE movements = argv[1];
   VALUE stepping = strcmp(rb_id2name(SYM2ID(argv[2])), "step") == 0;
 
-  VALUE board = rb_iv_get(piece, "@board");
+  VALUE board_ary = get_board_ary(piece);
   VALUE pos = rb_iv_get(piece, "@current_pos");
   int start_row = NUM2INT(rb_ary_entry(pos, 0));
   int start_col = NUM2INT(rb_ary_entry(pos, 1));
 
-  if (is_valid_pos(board, start_row, start_col, 0)) {
-    char color = get_color_at(board, start_row, start_col);
+  if (is_valid_pos(board_ary, start_row, start_col, 0)) {
+    char color = get_color_at(board_ary, start_row, start_col);
     for (int i = 0, n = RARRAY_LEN(movements); i < n; i++) {
       int *d = get_delta(rb_id2name(SYM2ID(rb_ary_entry(movements, i))));
       if (d != 0) {
         int dy = d[0], dx = d[1];
         for (int row = start_row + dy, col = start_col + dx
-          ; is_valid_pos(board, row, col, color)
+          ; is_valid_pos(board_ary, row, col, color)
           ; row += dy, col += dx) {
           add_move(moves, row, col);
-          if (stepping || is_occupied(board, row, col)) {
+          if (stepping || is_occupied(board_ary, row, col)) {
             break;
           }
         }
@@ -236,19 +258,19 @@ static VALUE get_pawn_moves(int argc, VALUE *argv, VALUE self) {
   VALUE moves = rb_ary_new();
   VALUE pawn = argv[0];
 
-  VALUE board = rb_iv_get(pawn, "@board");
+  VALUE board_ary = get_board_ary(pawn);
   VALUE pos = rb_iv_get(pawn, "@current_pos");
   int row = NUM2INT(rb_ary_entry(pos, 0));
   int col = NUM2INT(rb_ary_entry(pos, 1));
-  char color = get_color_at(board, row, col);
+  char color = get_color_at(board_ary, row, col);
   int step = (color == 'b') ? 1 : -1;
 
   //  pawn's step moves
 
   int on_home_row = (color == 'b' && row == 1) || (color == 'w' && row == 6);
   for (int dy = step, n = on_home_row ? 2 : 1; n--; dy += step) {
-    if (is_valid_pos(board, row + dy, col, color) &&
-      !is_occupied(board, row + dy, col)) {
+    if (is_valid_pos(board_ary, row + dy, col, color) &&
+      !is_occupied(board_ary, row + dy, col)) {
       add_move(moves, row + dy, col);
     } else {
       break;
@@ -257,12 +279,12 @@ static VALUE get_pawn_moves(int argc, VALUE *argv, VALUE self) {
 
   //  pawn's capture moves
 
-  if (is_valid_pos(board, row + step, col - 1, color) &&
-    is_occupied(board, row + step, col - 1)) {
+  if (is_valid_pos(board_ary, row + step, col - 1, color) &&
+    is_occupied(board_ary, row + step, col - 1)) {
     add_move(moves, row + step, col - 1);
   }
-  if (is_valid_pos(board, row + step, col + 1, color) &&
-    is_occupied(board, row + step, col + 1)) {
+  if (is_valid_pos(board_ary, row + step, col + 1, color) &&
+    is_occupied(board_ary, row + step, col + 1)) {
     add_move(moves, row + step, col + 1);
   }
 
@@ -279,15 +301,15 @@ static VALUE get_knight_moves(int argc, VALUE *argv, VALUE self) {
   VALUE moves = rb_ary_new();
   VALUE knight = argv[0];
 
-  VALUE board = rb_iv_get(knight, "@board");
+  VALUE board_ary = get_board_ary(knight);
   VALUE pos = rb_iv_get(knight, "@current_pos");
   int row = NUM2INT(rb_ary_entry(pos, 0));
   int col = NUM2INT(rb_ary_entry(pos, 1));
-  char color = get_color_at(board, row, col);
+  char color = get_color_at(board_ary, row, col);
 
   for (int i = 0, n = sizeof(KNIGHT_MOVES) / sizeof(KNIGHT_MOVES[0]); i < n; i++) {
     int dy = KNIGHT_MOVES[i][0], dx = KNIGHT_MOVES[i][1];
-    if (is_valid_pos(board, row + dy, col + dx, color)) {
+    if (is_valid_pos(board_ary, row + dy, col + dx, color)) {
       add_move(moves, row + dy, col + dx);
     }
   }
@@ -299,36 +321,22 @@ static VALUE get_knight_moves(int argc, VALUE *argv, VALUE self) {
 //  King nor Rook had been moved. Does not verify King won't move through/into
 //  Check.
 
-int rook_not_moved(VALUE ary, int row, int col) {
-  VALUE rook = rb_ary_entry(ary, row * 8 + col);
-  return strcmp(rb_obj_classname(rook), "Rook") == 0 &&
-    !RTEST(rb_iv_get(rook, "@moved"));
-}
-
-int not_occupied(VALUE ary, int row, int start_col, int end_col) {
-  for (int i = start_col; i <= end_col; i++) {
-    VALUE piece = rb_ary_entry(ary, row * 8 + i);
-    if (TYPE(rb_iv_get(piece, "@color")) != T_NIL) {
-      return 0;
-    }
-  }
-  return 1;
-}
-
 static VALUE get_castle_moves(int argc, VALUE *argv, VALUE self) {
   VALUE moves = rb_ary_new();
   VALUE king = argv[0];
 
   if (!RTEST(rb_iv_get(king, "@moved"))) {
-    VALUE ary = rb_iv_get(rb_iv_get(king, "@board"), "@board");
+    VALUE board_ary = get_board_ary(king);
     VALUE pos = rb_iv_get(king, "@current_pos");
     int row = NUM2INT(rb_ary_entry(pos, 0));
     int col = NUM2INT(rb_ary_entry(pos, 1));
-    if (col == 4) {
-      if (rook_not_moved(ary, row, 0) && not_occupied(ary, row, 1, col - 1)) {
+    if (col == 4) { // bug spray
+      if (rook_and_not_moved(board_ary, row, 0) &&
+        not_occupied(board_ary, row, 1, col - 1)) {
         add_move(moves, row, col - 2);
       }
-      if (rook_not_moved(ary, row, 7) && not_occupied(ary, row, col + 1, 6)) {
+      if (rook_and_not_moved(board_ary, row, 7) &&
+        not_occupied(board_ary, row, col + 1, 6)) {
         add_move(moves, row, col + 2);
       }
     }
@@ -341,11 +349,11 @@ static VALUE get_castle_moves(int argc, VALUE *argv, VALUE self) {
 
 static VALUE get_board_value(int argc, VALUE *argv, VALUE self) {
   int value = 0;
-  VALUE board = rb_iv_get(argv[0], "@board");
+  VALUE board_ary = rb_iv_get(argv[0], "@board");
   const char *player = rb_id2name(SYM2ID(argv[1]));
 
   for (int i = 0; i < 64; i++) {
-    value += get_piece_value(rb_ary_entry(board, i), i, player);
+    value += get_piece_value(rb_ary_entry(board_ary, i), i, player);
   }
 
   return INT2NUM(value);
