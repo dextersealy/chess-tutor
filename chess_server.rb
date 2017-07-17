5e9c48c6412b7e5f11f97128b2b3c5dd5fdbd007
Dexter Sealy <dextersealy@gmail.com>
2017-07-17 18:16:42 -0400

    Add option to initialize game from file

--- chess_server.rb
+++ chess_server.rb
@@ -4,6 +4,19 @@ require_relative 'controllers/chess_controller'
 require_relative 'lib/router'
 require_relative 'lib/static'
 
+options = {}
+OptionParser.new do |opts|
+  opts.on("-p", "--port PORT") do |port|
+    options[:port] = port
+  end
+  opts.on("-i", "--init BOARD", "Initialize board" ) do |board|
+    options[:board] = board
+  end
+  opts.on("-fBOARD", "Initialize board from file" ) do |filename|
+    options[:board] = File.read(filename).split("\n").first
+  end
+end.parse!
+
 router = Router.new
 router.draw do
   get Regexp.new("^/$"), ChessController, :init
@@ -13,16 +26,6 @@ router.draw do
   post Regexp.new("^/move$"), ChessController, :move
   get Regexp.new("^/move$"), ChessController, :make_move
 end
-
-options = {}
-OptionParser.new do |opts|
-  opts.on("-p", "--port PORT") do |port|
-    options[:port] = port
-  end
-  opts.on("-i", "--init BOARD", "Change the initial game board" ) do |board|
-    options[:board] = board
-  end
-end.parse!
 
 app = Proc.new do |env|
   req = Rack::Request.new(env)
